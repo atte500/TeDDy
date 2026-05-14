4df19a8cfd7c61e0d29bbb223187012416830c89
atte500 <raphael.a@hotmail.ch>
2026-05-14 11:31:35 +0200

    debug: probe xdist

--- spikes/debug/remote_probe.sh
+++ spikes/debug/remote_probe.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 echo "Starting Remote Probe..."
 
-# Disable xdist (-n 0) and run the test 10 times.
-# We also print the timeout just in case.
+# Enable xdist (-n 4) and run the entire file 10 times.
 for i in {1..10}; do
     echo "Run $i..."
-    # --timeout=15 to override the 5s timeout, to see if it just needs more time
-    poetry run pytest tests/suites/acceptance/test_session_resume_robustness.py::test_resume_auto_detects_latest_session -n 0 -vv -s --timeout=15 || {
+    poetry run pytest tests/suites/acceptance/test_session_resume_robustness.py -n 4 -vv --timeout=15 || {
         echo "Crash detected on run $i!"
         exit 1
     }
